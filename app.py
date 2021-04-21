10e9c63db57705639f0e7b59c34125350c394c84
Zhibo Liu <liuzb@iastate.edu>
2021-04-21 15:06:04 -0500

    Add Bootstrap n Models

--- app.py
+++ app.py
@@ -1,28 +1,20 @@
-import mysql.connector
 from flask import Flask, render_template, url_for, request, redirect
-#from flask_mysql_connector import MySQL
-#from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy import DDL, event
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from models.Users import Users
+from models.Recipes import Recipes
 
 
 app = Flask(__name__, template_folder='templates')
-#app.config['MYSQL_USER'] = 'b2838d74df3cc6'
-#app.config['MYSQL_PASSWORD'] = "1679be15" 
-#app.config['MYSQL_DATABASE'] = 'heroku_86594902d2459c3'
 
-
-
-
-db = mysql.connector.connect(
-    host = "us-cdbr-east-03.cleardb.com",
-    user = "b2838d74df3cc6",
-    password = "1679be15",
-    database = "heroku_86594902d2459c3"
-)
-
-cursor = db.cursor()
-
-    
+DB_URL = 'mysql+pymysql://b2838d74df3cc6:1679be15@us-cdbr-east-03.cleardb.com/heroku_86594902d2459c3?'
+engine = create_engine(DB_URL, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 
@@ -31,35 +23,21 @@ def index():
     return render_template('index.html')
 
 
+
 @app.route('/', methods = ['POST', 'GET'])
 def post():
 
-    db.reconnect(attempts = 1, delay = 0)
-
     if request.method == 'POST':
-        
-        new_recipe = Users(0, '')
-        cursor.execute("SELECT * FROM Recipes;")
-        rows = len(cursor.fetchall()) + 1
+
         text = request.form['content']
+        new_rec = Recipes(RECName = text)
+        session.add(new_rec)
+        session.commit()
 
-        SQL = "INSERT INTO Recipes VALUES(" + "{v1}, '{v2}');".format(v1 = rows, v2 = text)
-
-        try:
-            cursor.execute(SQL)
-            db.commit()
-            return render_template('index.html')
-
-        except:
-            return 'There was an issue adding a new recipe.'
+        return render_template('index.html')
 
     else:
         return render_template('index.html')
-
-
-
-
-    
 
 
 
